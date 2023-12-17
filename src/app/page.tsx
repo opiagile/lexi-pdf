420b2209d562933e934713f7379d9f1cf40cb77a
Francisco Araujo <franc@MacBook-Pro.local>
2023-12-17 19:37:58 -0400

    feat: implementação da autenticação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Link from 'next/link'
-import { ArrowRight } from 'lucide-react'
 import { buttonVariants } from '@/components/ui/button'
 import Image from 'next/image'
 
